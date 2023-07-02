d64e0f3815f80633a259376b4cc938ce89655adc
mcnanton <42379883+mcnanton@users.noreply.github.com>
2023-07-01 22:09:54 -0300

    Fix 6, add 7

--- sql_querys/6.sql
+++ sql_querys/6.sql
@@ -1,6 +1,6 @@
-SELECT DISTINCT isales.PromotionKey, COUNT(*) as total_uso_id_promo
+SELECT isales.PromotionKey, COUNT(*) as total_uso_id_promo
 FROM dbo.FactInternetSales isales
 LEFT JOIN dbo.DimProduct prod ON isales.ProductKey = prod.ProductKey
 WHERE isales.PromotionKey != '1'
-AND isales.SalesOrderNumber IN (SELECT sales.SalesOrderNumber FROM dbo.FactInternetSales sales WHERE YEAR(sales.OrderDate) IN (2010, 2011, 2012, 2013))
+AND YEAR(isales.OrderDate) IN (2010, 2011, 2012, 2013)
 GROUP BY isales.PromotionKey
