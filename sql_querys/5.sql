10a6c851107112098fe749e5bd4372149d4c9f7c
mcnanton <42379883+mcnanton@users.noreply.github.com>
2023-07-02 15:44:32 -0300

    Update q5

--- sql_querys/5.sql
+++ sql_querys/5.sql
@@ -7,18 +7,19 @@ LEFT JOIN dbo.DimProduct prod ON isales.ProductKey = prod.ProductKey
 LEFT JOIN dbo.DimProductSubcategory sub ON prod.ProductSubcategoryKey = sub.ProductSubcategoryKey
 LEFT JOIN dbo.DimProductCategory categ ON sub.ProductCategoryKey = categ.ProductCategoryKey
 WHERE YEAR(isales.OrderDate) IN (2010, 2011, 2012, 2013)),
+
 helper2 AS (
 SELECT SpanishProductCategoryName, con_sin_promo, COUNT(*) as total
 FROM helper1
 GROUP BY SpanishProductCategoryName, con_sin_promo),
+
 helper3 AS(
-SELECT
-  SpanishProductCategoryName,
-  SUM(CASE WHEN con_sin_promo = 'con_promocion' THEN total END) AS con_promocion,
-  SUM(CASE WHEN con_sin_promo = 'sin_promocion' THEN total END) AS sin_promocion,
-  SUM(total) AS total_categoria
+SELECT SpanishProductCategoryName,
+SUM(CASE WHEN con_sin_promo = 'con_promocion' THEN total END) AS productos_con_promocion,
+SUM(CASE WHEN con_sin_promo = 'sin_promocion' THEN total END) AS productos_sin_promocion,
+SUM(total) AS total_categoria
 FROM helper2
 GROUP BY SpanishProductCategoryName)
-SELECT SpanishProductCategoryName, con_promocion, sin_promocion, (con_promocion *100/total_categoria) as porc_con_promocion, (sin_promocion *100/total_categoria) as porc_sin_promocion
-FROM helper3
 
+SELECT SpanishProductCategoryName, total_categoria, productos_con_promocion, productos_sin_promocion, FORMAT((productos_con_promocion * 1.0 /total_categoria), 'P') as porc_con_promocion, FORMAT((productos_sin_promocion * 1.0 /total_categoria), 'P') as porc_sin_promocion
+FROM helper3
