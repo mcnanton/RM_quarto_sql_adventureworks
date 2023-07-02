53dce12dd22b810b48c765ee12087fe0cba25534
mcnanton <42379883+mcnanton@users.noreply.github.com>
2023-07-02 20:44:07 -0300

    Cambios q10, informe

--- sql_querys/10.sql
+++ sql_querys/10.sql
@@ -1,4 +1,5 @@
-WITH producto_venta_reseller AS (SELECT rsales.ProductKey, rsales.OrderQuantity, rsales.SalesOrderNumber, dr.ResellerName, rsales.PromotionKey
+WITH producto_venta_reseller AS 
+(SELECT rsales.ProductKey, rsales.OrderQuantity, rsales.SalesOrderNumber, dr.ResellerName, rsales.PromotionKey
 FROM dbo.FactResellerSales rsales
 LEFT JOIN dbo.DimReseller dr ON rsales.ResellerKey = dr.ResellerKey
 WHERE YEAR(rsales.OrderDate) IN (2010, 2011, 2012, 2013)),
@@ -11,11 +12,21 @@ total_vtas_reseller AS(
     FROM producto_venta_reseller
     GROUP BY ResellerName
 ),
+distinct_son AS (
+    SELECT DISTINCT SalesOrderNumber, ResellerName
+    FROM producto_venta_reseller 
+),
+total_ventas_realizadas_reseller AS(
+    SELECT ResellerName, COUNT(*) as ventas_realizadas
+    FROM distinct_son
+    GROUP BY ResellerName
+),
 total_items_reseller AS (
-    SELECT ResellerName, SUM(OrderQuantity) as total_productos
+    SELECT ResellerName, SUM(OrderQuantity) as total_unidades_vendidas
     FROM producto_venta_reseller
     GROUP BY ResellerName
 )
-SELECT tvr.ResellerName, total_ventas, tir.total_productos
+SELECT tvr.ResellerName, tir.total_unidades_vendidas, tvrr.ventas_realizadas
 FROM total_vtas_reseller tvr
 LEFT JOIN total_items_reseller tir ON tvr.ResellerName = tir.ResellerName
+LEFT JOIN total_ventas_realizadas_reseller tvrr ON tir.ResellerName = tvrr.ResellerName
