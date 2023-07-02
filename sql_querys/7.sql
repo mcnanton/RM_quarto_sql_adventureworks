64e5b126fecfc455e49ca0c539920c37f6a801bc
mcnanton <42379883+mcnanton@users.noreply.github.com>
2023-07-02 11:56:26 -0300

    Add top5 a q7, testeo gt

--- sql_querys/7.sql
+++ sql_querys/7.sql
@@ -2,7 +2,7 @@ WITH orderid_reseller AS (SELECT DISTINCT rsales.SalesOrderNumber, dr.ResellerNa
 FROM dbo.FactResellerSales rsales
 LEFT JOIN dbo.DimReseller dr ON rsales.ResellerKey = dr.ResellerKey
 WHERE YEAR(rsales.OrderDate) IN (2010, 2011, 2012, 2013))
-SELECT ResellerName, COUNT(*) as total_ventas
+SELECT TOP 5 ResellerName, COUNT(*) as total_ventas
 FROM orderid_reseller
 GROUP BY ResellerName
 ORDER BY total_ventas DESC
