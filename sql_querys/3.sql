14416853e4ef5a14bca48d72283ff55f266f4f5d
mcnanton <42379883+mcnanton@users.noreply.github.com>
2023-07-02 23:44:18 -0300

    Modif q3 a6, add informe

--- sql_querys/3.sql
+++ sql_querys/3.sql
@@ -1,6 +1,6 @@
 WITH reseller AS
 (
-SELECT frs.ProductKey, prod.EnglishProductName, frs.OrderQuantity, frs.UnitPrice, categ.SpanishProductCategoryName, 'Reseller Sales' AS CanalVenta, UnitPrice*OrderQuantity AS MontoVendido
+SELECT frs.ProductKey, prod.EnglishProductName as Product, frs.OrderQuantity, frs.UnitPrice, categ.SpanishProductCategoryName as Categoria, 'Reseller Sales' AS CanalVenta, UnitPrice*OrderQuantity AS MontoVendido
 FROM dbo.FactResellerSales frs
 LEFT JOIN dbo.DimProduct prod ON frs.ProductKey = prod.ProductKey
 LEFT JOIN dbo.DimProductSubcategory sub ON prod.ProductSubcategoryKey = sub.ProductSubcategoryKey
@@ -9,7 +9,7 @@ LEFT JOIN dbo.DimProductCategory categ ON sub.ProductCategoryKey = categ.Product
 
 internet AS 
 (
-SELECT fis.ProductKey, prod.EnglishProductName, fis.OrderQuantity, fis.UnitPrice, categ.SpanishProductCategoryName, 'Internet Sales' AS CanalVenta, UnitPrice*OrderQuantity AS MontoVendido
+SELECT fis.ProductKey, prod.EnglishProductName  as Product, fis.OrderQuantity, fis.UnitPrice, categ.SpanishProductCategoryName as Categoria, 'Internet Sales' AS CanalVenta, UnitPrice*OrderQuantity AS MontoVendido
 FROM dbo.FactInternetSales fis
 LEFT JOIN dbo.DimProduct prod ON fis.ProductKey = prod.ProductKey
 LEFT JOIN dbo.DimProductSubcategory sub ON prod.ProductSubcategoryKey = sub.ProductSubcategoryKey
@@ -18,31 +18,31 @@ LEFT JOIN dbo.DimProductCategory categ ON sub.ProductCategoryKey = categ.Product
 
 ambas AS 
 (
-SELECT ProductKey, SpanishProductCategoryName, EnglishProductName, OrderQuantity, UnitPrice, CanalVenta, MontoVendido
+SELECT Categoria,  Product, OrderQuantity, UnitPrice, CanalVenta, MontoVendido
 FROM reseller
 UNION ALL
-SELECT ProductKey, SpanishProductCategoryName, EnglishProductName, OrderQuantity, UnitPrice, CanalVenta, MontoVendido
+SELECT Categoria, Product, OrderQuantity, UnitPrice, CanalVenta, MontoVendido
 FROM internet
 ),
 
 tabla_sumario AS(
 SELECT *,
-SUM(OrderQuantity) OVER(PARTITION BY SpanishProductCategoryName, CanalVenta) AS n_prod_vendidos_categoria_canal,
-SUM(OrderQuantity) OVER(PARTITION BY SpanishProductCategoryName, CanalVenta, ProductKey) AS ventas_producto_categoria_canal,
-SUM(MontoVendido) OVER(PARTITION BY SpanishProductCategoryName, CanalVenta) AS monto_vendido_categoria_canal,
-SUM(MontoVendido) OVER(PARTITION BY SpanishProductCategoryName, CanalVenta, ProductKey) AS monto_vendido_producto_categoria_canal
+SUM(OrderQuantity) OVER(PARTITION BY Categoria, CanalVenta) AS n_vend_categ,
+SUM(OrderQuantity) OVER(PARTITION BY Categoria, CanalVenta, Product) AS n_vend_prod_categ,
+SUM(MontoVendido) OVER(PARTITION BY Categoria, CanalVenta) AS vtas_categ,
+SUM(MontoVendido) OVER(PARTITION BY Categoria, CanalVenta, Product) AS vtas_prod_categ
 FROM ambas),
 
 tabla_sumario_producto AS (
-SELECT DISTINCT CanalVenta, SpanishProductCategoryName, ProductKey, EnglishProductName, n_prod_vendidos_categoria_canal, ventas_producto_categoria_canal, monto_vendido_categoria_canal, monto_vendido_producto_categoria_canal
+SELECT DISTINCT CanalVenta, Categoria, Product, n_vend_categ, n_vend_prod_categ, vtas_categ, vtas_prod_categ
 FROM tabla_sumario),
 
 rn_tabla_sumario_producto AS(
-SELECT *, (monto_vendido_producto_categoria_canal*100/monto_vendido_categoria_canal) AS porc_monetario_categoria,
-ROW_NUMBER() OVER(PARTITION BY SpanishProductCategoryName, CanalVenta ORDER BY ventas_producto_categoria_canal DESC) AS ranking_cantidad_pvendidos_categoria
+SELECT *, (vtas_prod_categ*100/vtas_categ) AS porc_ingr_categoria,
+ROW_NUMBER() OVER(PARTITION BY Categoria, CanalVenta ORDER BY vtas_prod_categ DESC) AS ranking_cantidad_pvendidos_categoria
 FROM tabla_sumario_producto
 ) 
 SELECT *
 FROM rn_tabla_sumario_producto
-WHERE ranking_cantidad_pvendidos_categoria < 3
+WHERE ranking_cantidad_pvendidos_categoria < 4
 -- Uso intencional del row number en vez de dense_rank dado que en el canal internet sales muchos productos fueron vendidos solo 1 vez y todos cumplen el where de la línea anterior
