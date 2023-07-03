0d531431f3e831c0524dd07e09bbb2d770f630f6
mcnanton <42379883+mcnanton@users.noreply.github.com>
2023-07-03 11:30:26 -0300

    Add contenido a informe, ajuste q6

--- sql_querys/6.sql
+++ sql_querys/6.sql
@@ -4,7 +4,7 @@ LEFT JOIN dbo.DimProduct prod ON isales.ProductKey = prod.ProductKey
 WHERE isales.PromotionKey != '1'
 AND YEAR(isales.OrderDate) IN (2010, 2011, 2012, 2013)
 GROUP BY isales.PromotionKey)
-SELECT t1.PromotionKey, t1.total_uso_id_promo AS total_uso_promo, promo.SpanishPromotionName, promo.SpanishPromotionType Tipo, promo.SpanishPromotionCategory Categoria
+SELECT t1.PromotionKey Clave , t1.total_uso_id_promo AS n_veces_uso, promo.SpanishPromotionName, promo.SpanishPromotionType Tipo, promo.SpanishPromotionCategory Categoria
 FROM tabla1 t1
 LEFT JOIN dbo.DimPromotion promo on t1.PromotionKey = promo.PromotionKey
 ORDER BY total_uso_id_promo DESC
